35211e140bc590e1c4cc12ebe01fcaa89f318d38
drtnn <8david@inbox.ru>
2021-05-23 21:27:58 +0300

    Micro fixes for new server

--- models.py
+++ models.py
@@ -9,9 +9,9 @@ db = peewee.PostgresqlDatabase(os.environ['db_name'], host=os.environ['db_host']
 class User(peewee.Model):
 	user_id = peewee.IntegerField(verbose_name='User ID', unique=True)
 	username = peewee.CharField(
-		verbose_name='Username', max_length=50, null=True)
+		verbose_name='Username', max_length=128, null=True)
 	first_name = peewee.CharField(
-		verbose_name='First name', max_length=50, null=True)
+		verbose_name='First name', max_length=128, null=True)
 
 	def __str__(self):
 		return f'First name – {self.first_name}, User ID – {self.user_id}, Username – {self.username}'

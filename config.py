8bfd9ea9a3475fafee0571f905b7270ff6a5f857
drtnn <8david@inbox.ru>
2021-06-24 15:02:11 +0300

    reels support

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
-import os
-import telebot
+from environs import Env
+from telebot import TeleBot
 
+env = Env()
+env.read_env()
 
-ADMIN = 144589481
-bot = telebot.TeleBot(os.environ['token'])
+ADMINS = [int(user_id) for user_id in env.list('ADMINS')]
+BOT_TOKEN = env.str('BOT_TOKEN')
+COOKIE = env.str('COOKIE')
+DATABASE_URL = env.str('DATABASE_URL')
+
+bot = TeleBot(token=BOT_TOKEN)
